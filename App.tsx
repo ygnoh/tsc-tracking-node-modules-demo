25cb60e533d20a3a9ef707acc70216fa8ce95a13
yonggoo-noh <yonggoo.noh@navercorp.com>
2024-05-07 09:22:47 +0900

    Add a library

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {PieChart} from "react-native-gifted-charts";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <PieChart data={[]} />
       <StatusBar style="auto" />
     </View>
   );
